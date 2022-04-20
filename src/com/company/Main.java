fa68d7c5231c21eb9cb152ef88bb0b44f2d05c15
sunkilmoons <bcassidy50@gmail.com>
2022-04-20 08:41:19 -0500

    Load dictionary

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.io.File;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main {
 
+    static HashSet<String> dictionary = new HashSet<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world | Project #3");
+        String dictFile = "./data/aliceInWonderlandDictionary.txt";
+        loadDictionary(dictFile);
+    }
+
+    static void loadDictionary(String fileName) {
+        try {
+            File file = new File(fileName);
+
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String word = sc.nextLine();
+                dictionary.add(word);
+            }
+
+            System.out.printf("Successfully loaded dictionary. Words are: %s", dictionary.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
