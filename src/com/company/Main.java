b6ef652f61ef59d80926cf1313dc3658b9f359b6
sunkilmoons <bcassidy50@gmail.com>
2022-04-20 08:57:09 -0500

    add print result message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,28 @@
 package com.company;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     static HashSet<String> dictionary = new HashSet<>();
 
     public static void main(String[] args) {
+//        List<String> inputWords = // when not testing load input words...
+
+        String inputWord = "aliceinwonderland";
+
         String dictFile = "./data/aliceInWonderlandDictionary.txt";
         loadDictionary(dictFile);
+
+        // TODO: algorithm here...
+
+
+        printResult(inputWord, Collections.emptyList());
     }
 
     static void loadDictionary(String fileName) {
@@ -29,5 +41,14 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    static void printResult(String inputWord, List<String> splitWords) {
+        System.out.printf(
+                "%s can be split into %d AiW words: %s",
+                inputWord,
+                splitWords.size(),
+                String.join(", ", splitWords)
+                );
     }
 }
